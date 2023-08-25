1c7ca88139ab95f61e3cc8aa93ee03e8be2fb800
shivani <shivaninegi076@gmail.com>
2023-08-25 16:27:49 +0530

    frontend module added

---
+++ mernapp/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Home from './Screens/Home';
+import Login  from './Screens/Login';
+import {
+  BrowserRouter as Router ,
+  Routes,
+  Route,
+  Link
+
+
+}from "react-router-dom"
+
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ mernapp/src/Screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Footer from '../Components/Footer'
+import Navbar from '../Components/Navbar'
+
+export default function Home() {
+  return (
+    <>
+    <div><Navbar/></div>
+    <div>Body</div>
+    <div><Footer/></div>
+    </>
+  )
+}
